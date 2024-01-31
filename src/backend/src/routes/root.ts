e17684d248d316a144e207e1a04075891a3c5bc1
SilverYoCha <silveryocha@chastagnier.com>
2024-01-31 10:41:48 +0000

    Chat Client Is Working

--- src/backend/src/routes/root.ts
+++ src/backend/src/routes/root.ts
@@ -5,7 +5,18 @@ const root: FastifyPluginAsync = async (fastify, options): Promise<void> => {
     return { message: 'server up' };
   });
 
-  // TODO: create /chat endpoint
+  // reate /chat endpoint
+  fastify.post('/chat', async function (request, reply) {
+    const { messages } = request.body as any;
+    try {
+      return await fastify.chat.run(messages);
+    } catch (_error: unknown) {
+      const error = _error as Error;
+      fastify.log.error(error);
+      return reply.internalServerError(error.message);
+    }
+  });
+  
 };
 
 export default root;
